30142945d7239340aeef17184e2e09b1d8d2db10
Asier <asier@Personal.local>
2015-10-16 14:48:24 +0200

    PHP konprobaketak eta Login

--- erregistratu.php
+++ erregistratu.php
@@ -1,21 +1,19 @@
+
 <?php
-//LOCALHOST
-//$dbzerbitzaria = "localhost";
-//$dberabiltzailea = "root";
-//$dbpass = "";
-//$dbizena = "Quiz";
 
-
-
-//HOSTINGER
-//$dbzerbitzaria = "mysql.hostinger.es";
-//$dberabiltzailea = "u396496563_ehuws";
-//$dbpass = "wsehuws";
-//$dbizena = "u396496563_quiz";
-
-
+// LOCALHOST
+// $dbzerbitzaria = "localhost";
+// $dberabiltzailea = "root";
+// $dbpass = "";
+// $dbizena = "Quiz";
+// HOSTINGER
+// $dbzerbitzaria = "mysql.hostinger.es";
+// $dberabiltzailea = "u396496563_ehuws";
+// $dbpass = "wsehuws";
+// $dbizena = "u396496563_quiz";
 
 require 'konexioa.php';
+
 // ---------------------------------------------------------------------------------------------------------------------------------------
 // ---------------------------------------------------------------------------------------------------------------------------------------
 // ---------------------------------------------------------------------------------------------------------------------------------------
@@ -24,75 +22,74 @@ require 'konexioa.php';
 // ---------------------------------------------------------------------------------------------------------------------------------------
 // ---------------------------------------------------------------------------------------------------------------------------------------
 
-$izena = $_POST['izena'];
+$email = $_POST['posta'];
 
-// $argazkia = $_POST['argazkia'];
+if (filter_var($email, FILTER_VALIDATE_EMAIL) == false)
+	{
+echo ("<SCRIPT LANGUAGE='JavaScript'>
+    window.alert('Posta okerra :(')
+    window.location.href='signUp.html';
+    </SCRIPT>");
+	}
+  else
+	{
+	$izena = $_POST['izena'];
 
-$posta = $_POST['posta'];
-$pasahitza = $_POST['pasahitza'];
-$telefonoa = $_POST['telefonoa'];
-if($_POST['espezialitatea']=="Besterik"){
-	$espezialitatea = $_POST['besteespezialitatea'];
-}else{
-	$espezialitatea = $_POST['espezialitatea'];
-	
-}
+	// $argazkia = $_POST['argazkia'];
 
-$guztiak = "";
-if (isset($_POST['interesa'])) {
+	$posta = $_POST['posta'];
+	$pasahitza = $_POST['pasahitza'];
+	$telefonoa = $_POST['telefonoa'];
+	if ($_POST['espezialitatea'] == "Besterik")
+		{
+		$espezialitatea = $_POST['besteespezialitatea'];
+		}
+	  else
+		{
+		$espezialitatea = $_POST['espezialitatea'];
+		}
 
-    $interesa = $_POST['interesa'];
-    foreach($interesa as $result) {
-	$guztiak.= $result . ';';
-}
+	$guztiak = "";
+	if (isset($_POST['interesa']))
+		{
+		$interesa = $_POST['interesa'];
+		foreach($interesa as $result)
+			{
+			$guztiak.= $result . ';';
+			}
+		}
 
-} 
+	$besterik = $_POST['besterik'];
+	$fitxategiEdukia = null;
+	if ($_FILES['argazkia']['size'] > 0)
+		{
+		$fitxategi = $_FILES['argazkia']['name'];
+		$fitxategiarenIzena = $_FILES['argazkia']['tmp_name'];
+		$fitxategiaIriki = fopen($fitxategiarenIzena, 'r');
+		$fitxategiEdukia = fread($fitxategiaIriki, filesize($fitxategiarenIzena));
+		$fitxategiEdukia = addslashes($fitxategiEdukia);
+		fclose($fitxategiaIriki);
+		if (!get_magic_quotes_gpc())
+			{
+			$fitxategi = addslashes($fitxategi);
+			}
+		}
 
-$besterik = $_POST['besterik'];
-
-
-
-
-
-
-$fitxategiEdukia =null;
-if($_FILES['argazkia']['size'] > 0)
-{
-$fitxategi = $_FILES['argazkia']['name'];
-$fitxategiarenIzena  = $_FILES['argazkia']['tmp_name'];
-
-
-$fitxategiaIriki      = fopen($fitxategiarenIzena, 'r');
-$fitxategiEdukia = fread($fitxategiaIriki, filesize($fitxategiarenIzena));
-$fitxategiEdukia = addslashes($fitxategiEdukia);
-fclose($fitxategiaIriki);
-
-if(!get_magic_quotes_gpc())
-{
-    $fitxategi = addslashes($fitxategi);
-}
-
-
-
-
-}
-
-
-
-
-$sql = "INSERT INTO erabiltzaileak(izenabizen, posta, pasahitza, telefonoa, espezialitatea, interesak, besterik, argazkia)
+	$sql = "INSERT INTO erabiltzaileak(izenabizen, posta, pasahitza, telefonoa, espezialitatea, interesak, besterik, argazkia)
 VALUES ('$izena', '$posta', '$pasahitza','$telefonoa','$espezialitatea', '$guztiak','$besterik','$fitxategiEdukia')";
+	if (mysqli_query($konexioa, $sql))
+		{
+		echo "Erabiltzailea ondo erregistratua";
+		echo ("<br/>");
+		echo ("<a href=IkusiErabiltzaileak.php>Erabiltzaileak ikusi</a>");
+		}
+	  else
+		{
+		echo "Errorea: " . $sql . "<br />" . mysqli_error($konexioa);
+		}
 
-if (mysqli_query($konexioa, $sql)) {
-	echo "Erabiltzailea ondo erregistratua";
-	echo("<br/>");
-	echo("<a href=IkusiErabiltzaileak.php>Erabiltzaileak ikusi</a>");
-}
-else {
-	echo "Errorea: " . $sql . "<br />" . mysqli_error($konexioa);
-}
+	require 'konexioaItxi.php';
 
+	}
 
-
-require 'konexioaItxi.php';
 ?>
