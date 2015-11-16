4be208f387533deffddca1529ef1d9dca0eebf27
Asier <asier@Personal.local>
2015-11-16 09:56:53 +0100

    Zuzenketa

--- soapBezeroa.php
+++ soapBezeroa.php
@@ -1,13 +1,18 @@
 <?php
 require_once('nusoap/lib/nusoap.php');
-$client = new nusoap_client('http://sw14.hol.es/ServiciosWeb/comprobarmatricula.php');
+$client = new nusoap_client('http://sw14.hol.es/ServiciosWeb/comprobarmatricula.php',false);
 $client->soap_defencoding = 'UTF-8';
 $client->decode_utf8 = false;
-$result = $client->call('comprobar', 'proba@ikasle.ehu.es');
+$result = $client->call("comprobar", array('x'=>'$_GET[posta]'));
 //echo $client->debug_str;
 //echo $client->getError() . "\n";
 //print_r($result);
 //echo $client->request . "\n";
 echo $client->response . "\n";
-//echo $result;
+print_r($result);
+if($result=="NO"){
+echo("Ez zaude irakasgaian matrikulatua");	
+}else{
+	echo('<input type = "hidden" id="Matrikulatua" value="Bai"/>');
+}
 ?>
