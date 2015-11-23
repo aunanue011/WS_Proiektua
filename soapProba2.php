36d70f0bb4f810f99a1e46dc20366a98d093ce75
Asier <asier@Personal.local>
2015-11-23 06:53:20 +0100

    SOAP

--- soapProba2.php
+++ soapProba2.php
@@ -3,7 +3,7 @@ require_once("nusoap/lib/nusoap.php");
 $client = new nusoap_client('http://localhost/webSistemak/WS_Proiektua/egiaztatuPasahitza.php?wsdl');
 $client->soap_defencoding = 'UTF-8';
 $client->decode_utf8 = false;
-$result = $client->call("konprobatu",array('pass'=>'Arete'));
+$result = $client->call("konprobatu",array('pass'=>'Estayeguanoes'));
 //echo $client->debug_str;
 echo $client->getError() . "\n";
 //print_r($result);
