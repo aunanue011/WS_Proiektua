a2884fc432c702e0874e5d70c6bf4fa876959cfd
Asier <asier@Personal.local>
2015-11-23 06:53:23 +0100

    SOAP

--- egiaztatuPasahitza.php
+++ egiaztatuPasahitza.php
@@ -11,18 +11,36 @@ $server->configureWSDL('Pasahitza_Konprobatu',$namespace);
 
 function konprobatu($pass)
 {
-$handle = @fopen('./toppasswords.txt', "r");
-if ($handle) {
-  while (!feof($handle)) {
-    $entry_array = fgets($handle);
-    if ($entry_array == $pass) {
-      return "BALIOGABEA";
-      }
-    }
-  fclose($handle);
-  }
-return "BALIODUNA";
+//$handle = @fopen('./toppasswords.txt', "r");
+//if ($handle) {
+//  while (!feof($handle)) {
+//    $entry_array = fgets($handle);
+//    if ($entry_array == $pass) {
+//      return "BALIOGABEA";
+//      }
+//    }
+//  fclose($handle);
+//  }
+//return "BALIODUNA";
+$file = 'toppasswords.txt';
+$searchfor = $pass;
 
+// the following line prevents the browser from parsing this as HTML.
+//header('Content-Type: text/plain');
+
+// get the file contents, assuming the file to be readable (and exist)
+$contents = file_get_contents($file);
+// escape special characters in the query
+$pattern = preg_quote($searchfor, '/');
+// finalise the regular expression, matching the whole line
+$pattern = "/^.*$pattern.*\$/m";
+// search, and store all matching occurences in $matches
+if(preg_match_all($pattern, $contents, $matches)){
+   return "BALIOGABEA";
+}
+else{
+   return "BALIODUNA";
+}
 }
 
 $server->register('konprobatu',array("pass"=>"xsd:string"),array("return"=>"xsd:string"),"Pasahitza");
